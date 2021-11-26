647c3ce2b710a7262d02a1f6dcd34180fea38ff7
Lun-Dev <lunthedev@gmail.com>
2021-11-26 16:38:39 +0800

    Added: Basic framework

--- index.js
+++ index.js
@@ -1,30 +1,47 @@
-const gameBoard = (function() { // Module
-  const getContainer = document.querySelector(".grid-container")
-  
-  let _board = [...Array(9)].map(x => x = '');
-  let _draw = ''
+const getContainer = document.querySelector(".grid-container");
+const _board = [];
+let nextMove = 'X';
 
-  function method() {
-      for (const thing in _board) {
-          _draw += `<div class="cell">${_board[thing]}</div>`
-      }
-      getContainer.innerHTML = _draw
-  }
-  
-  return {
-  	method: method
-  }
-  	
-})();
-
-gameBoard.method()
-
-const createPlayers = (name, symbol) => { // Function Factory
-    const sample = () => { console.log("Hello")} // Auto goes to prototype
-    return { name, symbol, sample };
+function gameOver() {
+console.log("hi")
 }
 
-const user = createPlayers('Player One', 'x');
-const computer = createPlayers('Computer', 'o');
+function gameTie() {
+console.log("hi")
+}
 
-console.log(user.sample())
+function gameWon() {
+console.log("hi")
+}
+
+
+// const gameBoard = (function() { // Module
+//   const getContainer = document.querySelector(".grid-container")
+  
+//   let _board = [...Array(9)].map(x => x = '');
+//   let _draw = ''
+
+//   function method() {
+//       for (const thing in _board) {
+//           _draw += `<div class="cell">${_board[thing]}</div>`
+//       }
+//       getContainer.innerHTML = _draw
+//   }
+  
+//   return {
+//   	method: method
+//   }
+  	
+// })();
+
+// gameBoard.method()
+
+// const createPlayers = (name, symbol) => { // Function Factory
+//     const sample = () => { console.log("Hello")} // Auto goes to prototype
+//     return { name, symbol, sample };
+// }
+
+// const user = createPlayers('Player One', 'x');
+// const computer = createPlayers('Computer', 'o');
+
+// console.log(user.sample())
