5c337c00c5983f2117e2b833f395e095cc8d7c83
Lun-Dev <lunthedev@gmail.com>
2021-11-28 21:44:51 +0800

    Add: Won

--- index.js
+++ index.js
@@ -3,16 +3,42 @@
     const _board = [];
     let nextMove = "X"
 
-    function gameOver() {
-        _getContainer.style.display = "none"
+    function gameOver(message) {
+        console.log(message)
     }
 
     function gameTie() {
-    console.log("hi")
+        let notTie = true;
+        _board.forEach(({state}) => {
+            if (state == "") { 
+                notTie = false
+            };
+        })
+            return notTie;
     }
 
     function gameWon() {
-    console.log("hi")
+        const lines = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (
+                _board[a].state !== "" &&
+                _board[a].state === _board[b].state &&
+                _board[a].state === _board[c].state
+            ) {
+                return true;
+            }
+        }
+        return false;
     }
 
 
@@ -28,8 +54,8 @@ const squareBox = (element, index) => {
                     return false; // Make the square unclickable
                 };
                 element.querySelector('p').innerHTML = state; // fill the P with state (Either X or O)
-                if (gameWon()) return gameOver(`The Winner Is Player ${state}`)
-                if (gameTie()) return gameOver(`Draw`)
+                if (gameWon()) return gameOver(`The winner is player ${state}`)
+                if (gameTie()) return gameOver(`Tie`)
                 nextMove == "X" ? (nextMove = "O") : (nextMove = "X") // if nextMove = X, change it to O; otherwise, change it to X
             }
         }
@@ -45,8 +71,6 @@ const squareBox = (element, index) => {
         _board.push(square);
     }
 })();
-
-console.log(_board)
 
 // const gameBoard = (function() { // Module
 //   const getContainer = document.querySelector(".grid-container")
