5ffd00603a1aafa49df485d0767309defcc79c45
Lun-Dev <lunthedev@gmail.com>
2021-11-30 14:56:18 +0800

    Added: comments

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-(function() {
+(function() { // IIFE
     const _getContainer = document.querySelector(".grid-container");
     const _board = [];
     let nextMove = "X"
 
     function gameOver(message) {
-        alert(message)
+        _getContainer.style.display = "none"; // make board disapper
+        alert(message) // Declares the winner
     }
 
     function gameTie() {
         let notTie = true;
         _board.forEach(({state}) => { // check if each item in _board are filled
             if (state == "") { 
-                notTie = false
+                notTie = false // if fields are still blink, continue game
             };
         })
-            return notTie;
+            return notTie; // True and then display the message "Game Tie"
     }
 
     function gameWon() {
-        const lines = [
+        const lines = [ // winning combinations by checking the state
             [0,1,2],
             [3,4,5],
             [6,7,8],
@@ -31,31 +32,31 @@
         for (let i = 0; i < lines.length; i++) {
             const [a, b, c] = lines[i];
             if (
-                _board[a].state !== "" &&
-                _board[a].state === _board[b].state &&
+                _board[a].state !== "" && // if the state is not blink
+                _board[a].state === _board[b].state && // and that a b c equals the same symbol
                 _board[a].state === _board[c].state
             ) {
-                return true;
+                return true; // declare the winner
             }
         }
-        return false;
+        return false; // otherwise, continue game
     }
 
 
-const squareBox = (element, index) => {
-        return {
+const squareBox = (element, index) => { // factory function
+        return { // returns an object with the below properties
             element,
             index,
             state: '',
             clicked() {
-                this.state = nextMove; // state was blink, now it equals value in nextMove (Either X or O) 
+                this.state = nextMove; // state was blink, now it equals value in nextMove (Either X or O), the "this" is to bind to the current object
                 element.classList.remove("notClicked"); // Remove notClicked from div, since it is clicked
                 element.onclick = () => {
                     return false; // Make the square unclickable
                 };
                 element.querySelector('p').innerHTML = this.state; // fill the P with state (Either X or O)
-                if (gameWon()) return gameOver(`The winner is player ${this.state}`);
-                if (gameTie()) return gameOver(`Tie`);
+                if (gameWon()) return gameOver(`The winner is player ${this.state}`); // if true, delcare winner
+                if (gameTie()) return gameOver(`Tie`); // if true, declare tie
                 nextMove == "X" ? (nextMove = "O") : (nextMove = "X"); // if nextMove = X, change it to O; otherwise, change it to X
             }
         }
