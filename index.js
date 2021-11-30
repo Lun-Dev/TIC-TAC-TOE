9f645b24d716647b5cc8f83ff208df44bbdde424
Lun-Dev <lunthedev@gmail.com>
2021-11-30 14:49:42 +0800

    Added: Logic

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
     let nextMove = "X"
 
     function gameOver(message) {
-        console.log(message)
+        alert(message)
     }
 
     function gameTie() {
@@ -46,59 +46,30 @@ const squareBox = (element, index) => {
         return {
             element,
             index,
-            state: "",
+            state: '',
             clicked() {
-                state = nextMove; // state was blink, now it equals value in nextMove (Either X or O) 
+                this.state = nextMove; // state was blink, now it equals value in nextMove (Either X or O) 
                 element.classList.remove("notClicked"); // Remove notClicked from div, since it is clicked
                 element.onclick = () => {
                     return false; // Make the square unclickable
                 };
-                element.querySelector('p').innerHTML = state; // fill the P with state (Either X or O)
-                if (gameWon()) return gameOver(`The winner is player ${state}`)
-                if (gameTie()) return gameOver(`Tie`)
-                nextMove == "X" ? (nextMove = "O") : (nextMove = "X") // if nextMove = X, change it to O; otherwise, change it to X
+                element.querySelector('p').innerHTML = this.state; // fill the P with state (Either X or O)
+                if (gameWon()) return gameOver(`The winner is player ${this.state}`);
+                if (gameTie()) return gameOver(`Tie`);
+                nextMove == "X" ? (nextMove = "O") : (nextMove = "X"); // if nextMove = X, change it to O; otherwise, change it to X
             }
         }
     }
 
-    for (let i = 0; i < 9; i++) {
-        const div = document.createElement('div');
-        div.classList.add("square", "notClicked");
-        const square = squareBox(div, i);
-        div.onclick = () => { square.clicked() };
-        div.appendChild(document.createElement("p"))
-        _getContainer.appendChild(div);
-        _board.push(square);
-    }
+for (let i = 0; i < 9; i++) {
+    const div = document.createElement('div');
+    div.classList.add("square", "notClicked");
+    const square = squareBox(div, i);
+    div.onclick = () => { square.clicked() };
+    div.appendChild(document.createElement("p"))
+    _getContainer.appendChild(div);
+    _board.push(square);
+    console.log(_board)
+}
+
 })();
-
-// const gameBoard = (function() { // Module
-//   const getContainer = document.querySelector(".grid-container")
-  
-//   let _board = [...Array(9)].map(x => x = '');
-//   let _draw = ''
-
-//   function method() {
-//       for (const thing in _board) {
-//           _draw += `<div class="cell">${_board[thing]}</div>`
-//       }
-//       getContainer.innerHTML = _draw
-//   }
-  
-//   return {
-//   	method: method
-//   }
-  	
-// })();
-
-// gameBoard.method()
-
-// const createPlayers = (name, symbol) => { // Function Factory
-//     const sample = () => { console.log("Hello")} // Auto goes to prototype
-//     return { name, symbol, sample };
-// }
-
-// const user = createPlayers('Player One', 'x');
-// const computer = createPlayers('Computer', 'o');
-
-// console.log(user.sample())
